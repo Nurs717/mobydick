0bb439369755e06a2adec0c6d060e33a8d019999
Nurs717 <n.h.yergaliyev@gmail.com>
2021-04-28 00:12:01 +0600

    tryappend

--- main.go
+++ main.go
@@ -7,25 +7,45 @@ import (
 	"unicode"
 )
 
+var counter int
+
 func main() {
 	data, err := ioutil.ReadFile("mobydick.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	var n int
-	coter := 0
+	var test [][]byte
 	f := func(c rune) bool {
 		return !unicode.IsLetter(c)
 	}
-	a := bytes.FieldsFunc(data, f)
-	for i, word := range a {
-		coter = 0
-		for _, words := range a {
-			if bytes.EqualFold(word, words) {
-				coter++
-			}
-		}
-		fmt.Println(string(a[i]), coter)
+	words := bytes.FieldsFunc(data, f)
+	test = append(test, words[0])
+	test = loop(append(test, loop(words, test[0+counter])))
+
+	// LOOP:
+	// for {
+	// for _, word := range test {
+	// 	for _, words := range a {
+	// 		if !bytes.EqualFold(word, words) {
+	// 			// fmt.Printf("words: %s, ", word)
+	// 			if counter == 0 {
+	// 				test = append(test, words)
+	// 				counter++
+	// 			}
+	// 		}
+	// 	}
+	// 	counter = 0
+	// if word == nil {
+	// 	break LOOP
+	// }
+	// fmt.Println(string(a[i]), coter)
+	// }
+	// }
+	// fmt.Println(counter)
+
+	for _, val := range test {
+		fmt.Printf("%s, ", string(val))
 	}
 
 	for i, bytes := range data {
@@ -44,23 +64,36 @@ func main() {
 	}
 }
 
-func counter(data, word []byte) int {
-	var count int
-	var n int
-	for i, bytesd := range data {
-		if !(bytesd >= 'a' && bytesd <= 'z' || bytesd >= 'A' && bytesd <= 'Z' /* || bytes == ' ' || bytes == '\n'*/) {
-			if n == 0 {
-				n = i
-			} else {
-				e := data[n+1 : i]
-				n = i
-				res := bytes.EqualFold(e, word)
-				if res {
-					count++
-				}
-			}
-
+func loop(words [][]byte, test []byte) []byte {
+	var word2 []byte
+	for _, word := range words {
+		if !bytes.EqualFold(test, word) {
+			word2 = word
+			counter++
+			// break
+			return word2
 		}
 	}
-	return count
+	return word2
 }
+
+// func count(data, word []byte) int {
+// 	var count int
+// 	var n int
+// 	for i, bytesd := range data {
+// 		if !(bytesd >= 'a' && bytesd <= 'z' || bytesd >= 'A' && bytesd <= 'Z' /* || bytes == ' ' || bytes == '\n'*/) {
+// 			if n == 0 {
+// 				n = i
+// 			} else {
+// 				e := data[n+1 : i]
+// 				n = i
+// 				res := bytes.EqualFold(e, word)
+// 				if res {
+// 					count++
+// 				}
+// 			}
+
+// 		}
+// 	}
+// 	return count
+// }
